faa2c147bbf9293829809941a4f20df365d14527
Sergio <sergio64gr@gmail.com>
2015-03-04 18:27:38 +0100

    Cambios2

---
+++ Assets/Scripts/Cambia_gravedad.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cambia_gravedad : MonoBehaviour {
+
+	
+	void OnCollisionEnter2D(Collision2D target){	
+		if (target.transform.tag == "Player") {
+			Physics2D.gravity = Physics2D.gravity * -1;
+			Destroy(gameObject);
+			
+		}
+	}
+}
