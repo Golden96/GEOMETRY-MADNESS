71eef15ff3869e2e00f5b3411748f84d89f7215b
Sergio <sergio64gr@gmail.com>
2015-03-18 14:17:51 +0100

    Nivel

--- Assets/Scripts/Cambia_gravedad.cs
+++ Assets/Scripts/Cambia_gravedad.cs
@@ -2,11 +2,12 @@
 using System.Collections;
 
 public class Cambia_gravedad : MonoBehaviour {
-
+	public bool destroyOnCollision = false;
 	
 	void OnCollisionEnter2D(Collision2D target){	
 		if (target.transform.tag == "Player") {
-			Physics2D.gravity = Physics2D.gravity*-1;		
+			Physics2D.gravity = Physics2D.gravity*-1;	
+			Destroy(gameObject);
 		}
 	}
 }
